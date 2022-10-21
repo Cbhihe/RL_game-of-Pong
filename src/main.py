2ef2bd20b623f1a21ec2869906d7c3f2e6ca9a93
Cbhihe <cedric.bhihe@gmail.com>
2022-10-21 11:38:26 +0200

    ininitial commit

---
+++ src/main.py
@@ -0,0 +1,269 @@
+import control
+import pygame, os, time, pickle, sys
+import os.path
+import gzip
+from plot import plot_all
+import argparse
+
+from pong import *
+
+# Center window
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+SCREEN_SIZE = (640, 480)
+DRAW = True
+FPS = 200
+USE_KEYBOARD = False
+
+
+# Default configuration: training mode for 30 min
+TRAIN_DIR = 'train'
+TRAINING = True
+TRAIN_TIME = 30 * 60 # 30 min for training
+
+
+# The controller in use
+parser = argparse.ArgumentParser(description='Play or train a Pong game controller')
+
+parser.add_argument("-c", "--controller",
+	help="the name of the controller to use", type=str, required=True)
+
+parser.add_argument("-p", "--play", help="play the trained controller",
+    action="store_true")
+
+parser.add_argument("-k", "--keyboard", help="use the keyboard when playing",
+	action="store_true")
+
+parser.add_argument("-t", "--time", help="time in seconds for training",
+	type=int, default=TRAIN_TIME)
+
+parser.add_argument("-f", "--file", help="file used to restore training data",
+	type=str)
+
+args = parser.parse_args()
+
+if args.play:
+	TRAINING = False
+
+if args.keyboard:
+	USE_KEYBOARD = True
+	FPS = 60
+
+TRAIN_TIME = args.time
+
+try:
+	CONTROLLER = globals()[args.controller]()
+except KeyError:
+	print("Controller '{}' not found".format(args.controller))
+	exit(1)
+
+
+def get_train_path(controller, train_time):
+	name = controller.__class__.__name__
+	train_time = str(train_time)
+	fpath = os.path.join(TRAIN_DIR, name, train_time, 'data.gz')
+	return fpath
+
+
+if args.file != None:
+	TRAIN_FPATH = args.file
+else:
+	TRAIN_FPATH = get_train_path(CONTROLLER, args.time)
+
+
+
+if TRAINING:
+	print('Controller selected {}, train time {} seconds'.format(
+		args.controller, TRAIN_TIME))
+else:
+	print('Controller selected {}, play mode'.format(
+		args.controller))
+
+
+def load_controller(controller, train_time):
+	fpath = get_train_path(controller)
+	data = restore(fpath)
+	controller.restore(data)
+
+def tournament():
+	controllers = [CONTROLLER]
+	name = [c.__class__.__name__ for c in controllers]
+
+	# Reference controller for training
+	ref = control.Follower()
+	ref_name = ref.__class__.__name__
+
+	t = TRAIN_TIME
+
+	for c in controllers:
+		name = c.__class__.__name__
+		print('Training {}'.format(name), file=sys.stderr)
+		b = Board(None, SCREEN_SIZE, c, ref, display=False)
+
+		train_dir = '{}/{}/{}'.format(TRAIN_DIR, name, t)
+
+		if not os.path.exists(train_dir):
+		    os.makedirs(train_dir)
+
+		csv_file = os.path.join(train_dir, 'train.csv')
+
+		with open(csv_file, 'w') as csvfile:
+			c.log_file = csvfile
+			cdata, _ = train(b, c, ref, t)
+
+		savepath = os.path.join(train_dir, 'data.gz')
+		save(cdata, savepath)
+
+		print("Trained data saved in {}".format(savepath), file=sys.stderr)
+
+def measure(left, right, maxmatches=20):
+	"""A trained controller is played against a reference controller, and
+	performance is measured"""
+
+	b = Board(None, SCREEN_SIZE, left, right, display=False)
+	
+	frame = 1
+	delay = 10000
+	tic = time.time()
+
+	matches = -1
+
+	while b.matches < maxmatches:
+		if matches != b.matches:
+			matches = b.matches
+			print("Match {} of {}. Status: {}".format(
+				b.matches, maxmatches, b.win_matches))
+		b.update()
+		frame += 1
+
+	wins = b.win_matches
+	print(wins)
+
+
+def play(screen, controller, trainfile=None):
+
+	name = controller.__class__.__name__
+	if trainfile != None:
+		data = restore(trainfile)
+		controller.restore(data)
+
+	# Reference controller for playing
+	if USE_KEYBOARD:
+		ref = control.Keyboard()
+	else:
+		ref = control.Follower()
+
+	ref_name = ref.__class__.__name__
+
+	b = Board(screen, SCREEN_SIZE, controller, ref)
+	
+	clock = pygame.time.Clock()
+
+	frame = 1
+	delay = 10000
+	pause = False
+
+	draw = DRAW
+	tic = time.time()
+
+	print("Press f to toggle (f)ast drawing")
+	print("Press d to toggle (d)ebug drawings")
+	print("Press p to (p)ause")
+	print("Press q to (q)uit")
+
+	while b.run:
+
+		b.update(pause)
+		events = b.events
+
+		for event in events:
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_f:
+					#pygame.quit()
+					#exit()
+					draw = not draw
+				elif event.key == pygame.K_p:
+					pause = not pause
+
+		if draw or (not frame % delay):
+			b.draw()
+			pygame.display.update()
+			pygame.display.flip()
+			clock.tick(FPS)
+			delay = 200
+		else:
+			delay = 10000
+
+		if not frame % delay and not draw:
+			toc = time.time()
+			fps = frame/(toc-tic)
+			#print("FPS = {:.2f}".format(clock.get_fps()))
+			#print(b.marker)
+			#print("Real fps = {:.2f}".format(fps))
+			tic = time.time()
+			frame = 1
+
+		frame += 1
+	
+
+def train(b, left, right, t):
+	"Train 2 agents by playing the game t seconds"
+
+
+	frame = 1
+
+	tic = time.time()
+	toc = tic + t
+	while time.time() < toc:
+		b.update(False)
+		frame += 1
+
+	print('Trained for {:.2f} s with {} frames and {} matches'.format(
+		time.time() - tic, frame, b.matches), file=sys.stderr)
+
+	ldata = left.save()
+	rdata = right.save()
+
+	return (ldata, rdata)
+
+def save(data, fpath):
+	with gzip.open(fpath, 'wb') as f:
+		pickle.dump(data, f)
+
+def restore(fpath):
+	with gzip.open(fpath, 'rb') as f:
+		data = pickle.load(f)
+	
+	return data
+
+def main():
+
+	#left = control.QL2()
+	#right = control.Follower()
+
+	## Load trained controller
+	#load_controller(left)
+
+	#measure(left, right)
+	#return
+
+	if TRAINING:
+		tournament()
+	else:
+		pygame.font.init()
+		pygame.display.init()
+		pygame.display.set_caption('Controller {}'.format(args.controller))
+		screen = pygame.display.set_mode(SCREEN_SIZE)
+
+		# Play the only trained controller
+		c = CONTROLLER
+		dev_null = open(os.devnull, 'w')
+		c.log_file = dev_null
+		#trainfile = get_train_path(c)
+		play(screen, c, TRAIN_FPATH)
+
+
+if __name__ == '__main__':
+	main()
+	if TRAINING:
+		plot_all()
---
+++ src/pong.py
@@ -0,0 +1,535 @@
+# Encoding: utf-8
+
+import pygame
+from pygame.sprite import Sprite, collide_rect
+from pygame import Rect,Surface
+
+from control import *
+import numpy as np
+
+BOARD_MARGIN_X = 20
+PADDLE_MARGIN_X = 50
+PADDLE_MARGIN_Y = 20
+PADDLE_WIDTH = 10
+PADDLE_HEIGHT = 100
+PADDLE_RADIUS = 200
+PADDLE_TOP_SPEED = 3
+BALL_DIAMETER = 10
+BALL_INIT_SPEED = 5
+SCORE_MARGIN_X = 150
+SCORE_MARGIN_Y = 50
+
+class Board(Sprite):
+	def __init__(self, screen, size, cl, cr, display=True):
+		Sprite.__init__(self)
+		self.screen = screen
+		self.size = size
+		self.surf = Surface(size)
+		self.rect = self.surf.get_rect()
+	
+		self.pl = Paddle(self, PADDLE_MARGIN_X, PADDLE_MARGIN_Y, 'left')
+		self.pr = Paddle(self, PADDLE_MARGIN_X, PADDLE_MARGIN_Y, 'right')
+
+		self.ball = Ball(self, BALL_DIAMETER)
+
+		cl.set_board(self)
+		cr.set_board(self)
+
+		self.cl = cl
+		self.cr = cr
+
+		self.matches = 0
+		self.marker = {'left':0, 'right':0}
+		self.win_matches = {'left':0, 'right':0}
+		self.accum_points = {'left':0, 'right':0}
+		self.restart = False
+		self.player_scored = None
+
+		self.font = None
+		self.run = True
+		self.display = display
+		self.events = []
+
+	def draw_net(self):
+		rect = self.surf.get_rect()
+		top = rect.midtop
+		bottom = rect.midbottom
+		pygame.draw.line(self.surf, (150,150,150), top, bottom, 1)
+
+	def draw_score(self):
+		g = 200
+		color = (g,g,g)
+		ml = str(self.marker['left'])
+		mr = str(self.marker['right'])
+
+		# TODO: Use a good font
+		if self.font == None:
+			self.font = pygame.font.SysFont(None, 72)
+		surf_l = self.font.render(ml, True, color)
+		surf_r = self.font.render(mr, True, color)
+
+		rect_l = surf_l.get_rect()
+		rect_r = surf_r.get_rect()
+
+		# Place text in the middle of each player side
+		cx = self.rect.centerx
+		rect_l.centerx = cx/2
+		rect_r.centerx = self.rect.right - cx/2
+
+		rect_l.centery = SCORE_MARGIN_Y
+		rect_r.centery = SCORE_MARGIN_Y
+
+		self.surf.blit(surf_l, rect_l)
+		self.surf.blit(surf_r, rect_r)
+
+	
+	def update(self, pause=False):
+		if self.display:
+			self.events = pygame.event.get()
+			for event in self.events:
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_q:
+						#pygame.quit()
+						#exit()
+						self.run = False
+					if event.key == pygame.K_r:
+						# Restart counters
+						self.marker = {'left':0, 'right':0}
+
+		if self.marker['left'] >= 100 or self.marker['right'] >= 100:
+			if self.marker['left'] > self.marker['right']:
+				self.win_matches['left'] += 1
+			else:
+				self.win_matches['right'] += 1
+
+			self.marker = {'left':0, 'right':0}
+			self.matches += 1
+
+		if pause: return
+
+		self.ball.update()
+
+		self.cl.update()
+		self.cr.update()
+
+		self.pl.update()
+		self.pr.update()
+
+		if self.restart:
+			self.ball.restart()
+			self.restart = False
+			self.player_scored = None
+			#print(self.marker)
+
+		#if self.marker['left'] >= 1000 or self.marker['right'] >= 1000:
+		#	print(self.marker)
+		#	self.run = False
+
+	def draw(self):
+		# Clear the board
+		self.surf.fill((0,0,0))
+
+		# Draw elements
+		self.draw_net()
+		self.draw_score()
+
+		self.pl.draw()
+		self.pr.draw()
+
+		self.ball.draw()
+		self.cl.draw()
+		self.cr.draw()
+
+
+		# Finally blit into the screen
+		self.screen.blit(self.surf, dest=(0,0))
+
+	def score(self, player):
+		self.marker[player] += 1
+		self.accum_points[player] += 1
+		self.restart = True
+		self.player_scored = player
+
+	def get_accum_points(self, controller, me=True):
+		pl = self.accum_points['left']
+		pr = self.accum_points['right']
+
+		if me:
+			if controller == self.cl:
+				return pl
+			else:
+				return pr
+		else:
+			if controller == self.cl:
+				return pr
+			else:
+				return pl
+
+	def get_ball_position(self, controller):
+		if controller == self.cl:
+			return self.ball.position
+
+		# Mirror table
+		bx, by = self.ball.position
+		w, h = self.size
+
+		bx = w - bx - 1
+		
+		return np.array([bx, by])
+
+	def get_ball_speed(self, controller):
+		if controller == self.cl:
+			return self.ball.speed
+
+		# Mirror ball speed
+		vx, vy = self.ball.speed
+
+		return np.array([-vx, vy])
+
+	def get_ball_top_speed(self, controller):
+		return self.ball.top_speed
+
+	def get_player_position(self, controller, me=True):
+		if me:
+			if controller == self.cl:
+				return self.pl.y
+			else:
+				return self.pr.y
+		else:
+			if controller == self.cl:
+				return self.pr.y
+			else:
+				return self.pl.y
+
+	def set_player_speed(self, controller, speed):
+		if controller == self.cl:
+			self.pl.set_speed(speed)
+		else:
+			self.pr.set_speed(speed)
+
+	def get_player_speed(self, controller):
+		if controller == self.cl:
+			return self.pl.vy
+		else:
+			return self.pr.vy
+
+	def get_debug_zone(self, controller):
+		w,h = self.size
+		rect = Rect((0, PADDLE_MARGIN_Y), (PADDLE_MARGIN_X, h-2*PADDLE_MARGIN_Y))
+
+		if controller == self.cl:
+			rect.right = PADDLE_MARGIN_X
+			return rect
+			
+		else:
+			rect.left = w - PADDLE_MARGIN_X
+			return rect
+
+	def get_paddle_rect(self, controller):
+		if controller == self.cl:
+			return self.pl.rect
+
+		# Mirror paddle rect
+		size = (PADDLE_WIDTH, PADDLE_HEIGHT)
+		topleft = (PADDLE_MARGIN_X, PADDLE_MARGIN_Y)
+		rect = Rect(topleft, size)
+		rect.top = self.pr.rect.top
+		
+		return rect
+
+	def get_marker(self, controller):
+		ml = self.marker['left']
+		mr = self.marker['right']
+
+		if controller == self.cl:
+			return {'me':ml, 'opponent':mr}
+		else:
+			return {'me':mr, 'opponent':ml}
+
+	def get_player_scored(self, controller):
+		side = self.player_scored
+
+		if not side: return None
+
+		if controller == self.cl:
+			if side == 'left':
+				return 'me'
+			else:
+				return 'opponent'
+
+		else:
+			if side == 'right':
+				return 'me'
+			else:
+				return 'opponent'
+
+	def get_paddle_top_speed(self, controller):
+		return PADDLE_TOP_SPEED
+
+	def get_paddle_status(self, controller):
+		if controller == self.cl:
+			return self.pl.status
+		else:
+			return self.pr.status
+		
+			
+class Paddle(Sprite):
+	def __init__(self, board, hmargin, vmargin, side):
+		Sprite.__init__(self)
+		self.board = board
+		self.board_size = board.surf.get_size()
+		self.margin = (hmargin, vmargin)
+
+		self.size = (PADDLE_WIDTH, PADDLE_HEIGHT)
+		self.rect = Rect((0, 0), self.size)
+
+		# 1-pixel wide collision rect placed at the center side of the paddle
+		self.crect = Rect((0, 0), (1, PADDLE_HEIGHT))
+		w,h = self.board_size
+
+		# Place the paddle at the top
+		if side == 'left':
+			self.rect.topleft = (hmargin, vmargin)
+			self.crect.topright = self.rect.topright
+		else:
+			self.rect.topright = (w - hmargin, vmargin)
+			self.crect.topleft = self.rect.topleft
+
+		wboard, hboard = self.board_size
+		wsurface = PADDLE_WIDTH
+		hsurface = hboard - vmargin*2
+		surf_size = (wsurface, hsurface)
+		self.surf = Rect(self.rect.topleft, surf_size)
+
+		self.side = side
+
+		# The center y position of the paddle in floating point
+		self.y = self.rect.centery
+
+		# Paddle vertical speed
+		self.vy = 0.0
+
+		self.top_speed = PADDLE_TOP_SPEED
+		self.status = None
+
+	def set_speed(self, vy):
+		if vy > self.top_speed:
+			vy = self.top_speed
+		if vy < -self.top_speed:
+			vy = -top_speed
+
+		self.vy = vy
+
+	def set_position(self, y):
+		rect = self.rect
+
+		rect.centery = y
+		top = self.surf.top
+		bottom = self.surf.bottom
+
+		if rect.top < top:
+			rect.top = top
+			y = rect.centery
+		if rect.bottom > bottom:
+			rect.bottom = bottom
+			y = rect.centery
+
+		# Place also the crect
+		if self.side == 'left':
+			self.crect.topright = self.rect.topright
+		else:
+			self.crect.topleft = self.rect.topleft
+
+		self.y = y
+
+	def status_ball(self, ball):
+		ball_rect = ball.rect
+		bx, by = ball_rect.center
+		px, py = self.crect.center
+
+		if self.crect.colliderect(ball_rect):
+			self.status = 'collision'
+		elif self.side == 'left' and bx < px:
+			self.status = 'lost'
+		elif self.side == 'right' and bx > px:
+			self.status = 'lost'
+		else:
+			self.status = None
+
+	def update(self):
+		self.set_position(self.y + self.vy)
+
+	def draw(self):
+		# Draw the paddle in the rect position
+		g = 200
+		self.board.surf.fill((g,g,g), self.rect)
+		#self.board.surf.fill((255,127,127), self.crect)
+
+
+class Ball(Sprite):
+	def __init__(self, board, diameter):
+		Sprite.__init__(self)
+		self.board = board
+		self.diameter = diameter
+		self.size = (diameter, diameter)
+		self.ball = Surface(self.size)
+		self.ball.fill((0,255,0))
+		self.rect = Rect(self.board.rect.center, self.size)
+
+		w,h = self.board.surf.get_size()
+
+		# The ball center position in floating point
+		self.position = (w/2, h/2)
+
+		# Speed is measured in pixels / frame
+		self.speed = np.array([3.0, 3.3])
+
+		self.top_speed = diameter
+		self.init_speed = BALL_INIT_SPEED
+
+	def check_miss(self):
+		# Test if the ball was missed by any paddle
+
+		x, y = self.rect.center
+		pr = self.board.pr
+
+		pl = self.board.pl
+
+		pl.status_ball(self)
+		pr.status_ball(self)
+
+		if pl.status == 'lost':
+			self.board.score('right')
+		elif pr.status == 'lost':
+			self.board.score('left')
+
+		
+	def restart(self):
+		# Put the ball in the center again
+		self.rect.center = self.board.rect.center
+		self.position = self.rect.center
+
+		side = self.board.player_scored
+		bw, bh = self.board.size
+
+		# Select a random angle such that the ball goes directly to the paddle
+		# area (margins are ignored?)
+		max_slope = (bh/2) / (bw/2)
+		max_angle = np.arctan(max_slope)
+		angle = np.random.uniform(-max_angle, max_angle)
+
+		# If the winner was the right one, the ball should move to the left
+		if side == 'right':
+			angle = np.pi + angle
+
+
+		# Now we have the angle and the vector length
+		phi = angle
+		rho = self.init_speed
+		vx = rho * np.cos(phi)
+		vy = rho * np.sin(phi)
+
+		speed = np.array([vx, vy])
+		
+		self.set_speed(speed)
+
+	def collide_paddles(self):
+		pr = self.board.pr
+		pl = self.board.pl
+
+		v = self.speed
+		vx, vy = v
+
+		#if collide_rect(self, pr) or collide_rect(self, pl):
+		if collide_rect(self, pl) and vx < 0:
+
+			cx, cy = pl.rect.center
+			cx -= PADDLE_RADIUS
+
+		elif collide_rect(self, pr) and vx > 0:
+
+			cx, cy = pr.rect.center
+			cx += PADDLE_RADIUS
+
+		else:
+			return
+
+		bx, by = self.rect.center
+
+		nx = bx - cx
+		ny = by - cy
+
+		n = np.array([nx, ny])
+
+		# The new axis of reflectios for the trajectory
+		n = n / np.linalg.norm(n)
+
+		v = v - 2 * np.dot(v, n) * n
+
+
+		# Avoid the ball to move close to 90 and 270 degrees (pi/2 and 3 pi/2)
+
+		#angle_limit = 0.2 # About 11.5 degrees
+		#if abs(angle - pi/2) < 0.2:
+		#	if angle < pi/2:
+		#		angle = pi/2 - angle_limit
+		#	else:
+		#		angle = pi/2 + angle_limit
+
+		#if abs(angle - 3*pi/2) < 0.2:
+		#	if angle < 3*pi/2:
+		#		angle = 3*pi/2 - angle_limit
+		#	else:
+		#		angle = 3*pi/2 + angle_limit
+
+		#vx = -vx
+		
+		# Speed up a bit the ball
+		v *= 1.1
+
+		self.set_speed(v)
+
+	def set_speed(self, speed):
+		speed = np.array(speed)
+
+		abs_speed = np.linalg.norm(speed)
+
+		# Avoid the ball from exceeding the top speed
+		if abs_speed > self.top_speed:
+			speed/abs_speed 
+			speed *= self.top_speed/abs_speed
+			#print('Ball limit speed reached')
+
+		vx,vy = speed
+		if np.abs(vx) < 1.0:
+			vx = np.sign(vx) * 1.0
+
+		speed = np.array([vx, vy])
+
+		self.speed = speed
+
+	def update(self):
+		w,h = self.board.surf.get_size()
+
+		self.check_miss()
+		self.collide_paddles()
+
+		px, py = self.position
+		vx, vy = self.speed
+		px += vx
+		py += vy
+
+		if px > w or px < 0:
+			vx = -vx
+
+		if py > h or py < 0:
+			vy = -vy
+
+		self.speed = (vx, vy)
+		self.position = (px, py)
+
+		self.rect.center = (px, py)
+
+	def draw(self):
+		self.board.surf.blit(self.ball, self.rect)
+
